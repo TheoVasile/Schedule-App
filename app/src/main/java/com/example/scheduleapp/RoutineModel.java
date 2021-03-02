0be24d9f334d11131972f28de742a659c0028deb
TheoVasile <theo.vasile50@yahoo.com>
2021-03-02 13:53:19 -0500

    added getters for RoutineModel

--- app/src/main/java/com/example/scheduleapp/RoutineModel.java
+++ app/src/main/java/com/example/scheduleapp/RoutineModel.java
@@ -2,15 +2,47 @@ package com.example.scheduleapp;
 
 public class RoutineModel {
 
-    String title;
-    String description;
-    String startTime;
-    String endTime;
+    private String title;
+    private String description;
+    private String startTime;
+    private String endTime;
 
-    public void RoutineModel(String title, String description, String startTime, String endTime){
+    public RoutineModel(String title, String description, String startTime, String endTime){
         this.title = title;
         this.description = description;
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStartTime() {
+        return this.startTime;
+    }
+
+    public int getStartHour() {
+        return Integer.valueOf(this.startTime.split(":")[0]);
+    }
+
+    public int getStartMinute() {
+        return Integer.valueOf(this.startTime.split(":")[1]);
+    }
+
+    public String getEndTime() {
+        return this.endTime;
+    }
+
+    public int getEndHour() {
+        return Integer.valueOf(this.endTime.split(":")[0]);
+    }
+
+    public int getEndMinute() {
+        return Integer.valueOf(this.endTime.split(":")[1]);
     }
 }
