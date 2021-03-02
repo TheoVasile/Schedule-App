c519d65cb357f6847467b2cfdf65dc698101ab6e
TheoVasile <theo.vasile50@yahoo.com>
2021-03-02 13:24:09 -0500

    activity_main now has grid lines for each hour of the day

--- app/src/main/java/com/example/scheduleapp/MainActivity.java
+++ app/src/main/java/com/example/scheduleapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.scheduleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout timeLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeLinearLayout = findViewById(R.id.time_linear_layout);
+
+        for (int i = 0; i < 24; i++) {
+            LinearLayout timeBar = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.time_bar, timeLinearLayout, false);
+            TextView timeBarText = timeBar.findViewById(R.id.time_bar_text);
+            String text = String.valueOf(i) + ":00";
+            timeBarText.setText(text);
+
+            timeLinearLayout.addView(timeBar);
+        }
+
     }
 }
