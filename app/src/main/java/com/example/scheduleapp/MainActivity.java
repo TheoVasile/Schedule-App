81b2de6ad6c7190e322def2f56176a951ec2231d
TheoVasile <theo.vasile50@yahoo.com>
2021-03-02 14:08:50 -0500

    routines can be added to the activity_main layout

--- app/src/main/java/com/example/scheduleapp/MainActivity.java
+++ app/src/main/java/com/example/scheduleapp/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout timeLinearLayout;
+    LinearLayout routineLinearLayout;
+    ArrayList<RoutineModel> routineModelArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timeLinearLayout = findViewById(R.id.time_linear_layout);
+        routineLinearLayout = findViewById(R.id.routine_linear_layout);
+        routineModelArrayList = new ArrayList<RoutineModel>();
+        routineModelArrayList.add(new RoutineModel("Workout", "Morning bulk workout.", "10:00", "12:00"));
 
         for (int i = 0; i < 24; i++) {
             LinearLayout timeBar = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.time_bar, timeLinearLayout, false);
@@ -25,6 +33,21 @@ public class MainActivity extends AppCompatActivity {
             timeBarText.setText(text);
 
             timeLinearLayout.addView(timeBar);
+        }
+
+        for (RoutineModel routineModel : routineModelArrayList) {
+            RelativeLayout routineCard = (RelativeLayout) LayoutInflater.from(this).inflate(R.layout.routine_card, routineLinearLayout, false);
+
+            TextView routineTitle = routineCard.findViewById(R.id.routine_title);
+            routineTitle.setText(routineModel.getTitle());
+
+            TextView routineStartTime = routineCard.findViewById(R.id.routine_start_time);
+            routineStartTime.setText(routineModel.getStartTime());
+
+            TextView routineEndTime = routineCard.findViewById(R.id.routine_end_time);
+            routineEndTime.setText(routineModel.getEndTime());
+
+            routineLinearLayout.addView(routineCard);
         }
 
     }
