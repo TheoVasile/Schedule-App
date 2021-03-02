9596d4a0710944e7cccc7e5356ad2769630d3099
TheoVasile <theo.vasile50@yahoo.com>
2021-03-02 13:48:22 -0500

    created RoutineModel attributes

--- app/src/main/java/com/example/scheduleapp/RoutineModel.java
+++ app/src/main/java/com/example/scheduleapp/RoutineModel.java
@@ -1,4 +1,16 @@
 package com.example.scheduleapp;
 
 public class RoutineModel {
+
+    String title;
+    String description;
+    String startTime;
+    String endTime;
+
+    public void RoutineModel(String title, String description, String startTime, String endTime){
+        this.title = title;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 }
